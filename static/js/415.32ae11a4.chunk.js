2df11a7e84d8e55895e1da7696a30f1d71edbad8
VikaVelhan <106451622+VikaVelhan@users.noreply.github.com>
2023-03-17 16:48:27 +0000

    Deploying to gh-pages from @ VikaVelhan/goit-react-hw-08-phonebook@19b0b0ddb7ec351b6c61283d4eb727ba6059d7a8 🚀

---
+++ static/js/415.32ae11a4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,t,n){n.r(t),n.d(t,{default:function(){return l}});var i=n(3329),r={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};function l(){return(0,i.jsx)("div",{style:r.container,children:(0,i.jsx)("h1",{style:r.title,children:"Welcome to the Phonebook! Please, register or log in."})})}}}]);
+//# sourceMappingURL=415.32ae11a4.chunk.js.map
